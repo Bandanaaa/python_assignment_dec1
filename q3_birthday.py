f007d4c5ae8f4b09de80c3a2e00ae6955c832e79
Tejas <tejaspant55@gmail.com>
2017-12-03 17:43:11 +0545

    Birthday song for a friend

---
+++ q3_birthday.py
@@ -0,0 +1,5 @@
+def birthday(name):
+    print("Happy birthday to you \n happy birthday {:s} \n happy birthday to you!!!  ".format(name))
+
+friend = str(input("Enter your friend's name: "))
+birthday(friend)
